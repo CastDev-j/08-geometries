f540b012aa9cd4a3b728e0e699dada286576ba08
AndDev <23031429@itcelaya.edu.mx>
2025-05-31 19:12:40 -0600

    Add lil-gui for GUI controls and enhance mesh properties in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,33 @@
 import "./style.css";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import * as THREE from "three";
+import GUI from "lil-gui";
+import gsap from "gsap";
+
+const gui = new GUI({
+  // width: 300,
+  autoPlace: true,
+  title: "Cool Debug Panel",
+  closeFolders: true,
+})
+  .close();
+  // .hide();
+
+const cubeFolder = gui.addFolder("Cube");
+
+const debugObject = {
+  color: "#a778d8",
+  isAnimated: true,
+  speed: 1,
+  subdivisions: 1,
+  spin: () => {
+    gsap.to(mesh.rotation, {
+      duration: 1,
+      y: mesh.rotation.y + Math.PI * 2,
+      ease: "power1.inOut",
+    });
+  },
+};
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
@@ -14,13 +41,51 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
+  color: debugObject.color,
   wireframe: true,
 });
 
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
+
+cubeFolder
+  .add(mesh.position, "y")
+  .min(-1)
+  .max(1)
+  .step(0.01)
+  .name("Mesh Y Position");
+
+cubeFolder.add(mesh, "visible");
+cubeFolder.add(mesh.material, "wireframe");
+
+// cubeFolder.add(debugObject, "isAnimated").name("Animate Mesh");
+// cubeFolder.add(debugObject, "speed").min(0).max(5).step(0.01).name("Animation Speed");
+
+cubeFolder.addColor(debugObject, "color").onChange(() => {
+  mesh.material.color.set(debugObject.color);
+});
+
+cubeFolder
+  .add(debugObject, "subdivisions")
+  .min(1)
+  .max(32)
+  .step(1)
+  .name("Subdivisions")
+  .onFinishChange(() => {
+    const { subdivisions } = debugObject;
+    mesh.geometry.dispose();
+    mesh.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      subdivisions,
+      subdivisions,
+      subdivisions
+    );
+  });
+
+cubeFolder.add(debugObject, "spin").name("Spin Mesh");
 
 const axesHelper = new THREE.AxesHelper(1);
 scene.add(axesHelper);
@@ -49,7 +114,10 @@ const tick = () => {
 
   window.requestAnimationFrame(tick);
 
-  // mesh.rotation.y = elapsedTime;
+  // animations
+  // if (debugObject.isAnimated) {
+  //   mesh.rotation.y = elapsedTime * debugObject.speed;
+  // }
 
   // camera.lookAt(mesh.position);
 
@@ -78,17 +146,10 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-// Handle canvas double-click to toggle fullscreen
+// Show the GUI
 
-window.addEventListener("dblclick", () => {
-
-  if (!document.fullscreenElement) {
-    canvas.requestFullscreen().catch((err) => {
-      console.error(
-        `Error attempting to enable fullscreen mode: ${err.message}`
-      );
-    });
-  } else {
-    document.exitFullscreen();
+document.addEventListener("keydown", (event) => {
+  if (event.key === "g") {
+    gui.show(gui._hidden);
   }
 });
